56437147aae0a0a805e4457e6f66e79d94718699
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-25 13:56:11 +0000

    finish Sum All task

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        let min = a, max = b;
+        if (a > b) {
+            min = b;
+            max = a;
+        }
 
+        let total = 0;
+        let i = min;
+        for (i; i <= max; i++) {
+            total += i;
+        }
+
+        return total;
+    }
+    else {
+        return 'ERROR';
+    }
 }
 
 module.exports = sumAll
