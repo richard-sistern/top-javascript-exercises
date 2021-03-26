edae9fb29e6fc8c1a7745e0aa0cbe6eb6b0e7ef0
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-26 10:57:32 +0000

    finish celsius to fahrenheit conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -5,8 +5,10 @@ const ftoc = function (fahrenheit) {
 }
 
 // celsius to fahrenheit
-const ctof = function() {
-
+const ctof = function (celsius) {
+ // [°C] × ​9⁄5 + 32
+  let fahrenheit = (celsius * 1.8) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 }
 
 module.exports = {
