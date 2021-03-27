7452f0cce64dc94c9bb61dcb004d9432c515e6c4
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-27 07:39:37 +0000

    finish palindrome task

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    cleanString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    reverseString = cleanString.split("").reverse().join("")
+    return cleanString == reverseString;
 }
 
 module.exports = palindromes
